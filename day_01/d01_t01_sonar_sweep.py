a5c5d7cc254335ab5fcecd6ca42e975adb39aeb5
MV <64264846+bl255@users.noreply.github.com>
2022-11-17 17:03:53 +0100

    Added folder with solution and input for the first task of day 1.

---
+++ day_01/d01_t01_sonar_sweep.py
@@ -0,0 +1,8 @@
+input_1 = "input_01.txt"
+
+with open(input_1, mode="r") as text_file:
+    depth_list = [line.strip() for line in text_file.readlines()]
+
+decreased = sum((depth_list[num] < depth_list[num + 1] for num, item in enumerate(depth_list[:-1])))
+
+print(decreased)
